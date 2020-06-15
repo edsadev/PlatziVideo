a6b1e16a51197b1d6340f6b2a284151faafbe712
Edmundo0994 <esala.094@gmail.com>
2020-06-15 16:27:12 -0500

    Agregado eslint con reglas para el código

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.js', /*referencia al archivo principal*/
-  output: {   /*resolve permite detectar el __dirname directorio en el que estamos y pasandole un directorio para guardar los archivos*/
+  output: { /*resolve permite detectar el __dirname directorio en el que estamos y pasandole un directorio para guardar los archivos*/
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js', /* filename es el nombre del archivo final */
   }, /*output donde se guardan los archivos resultantes cuando se haga la compilación*/
@@ -17,34 +17,34 @@ module.exports = {
         test: /\.(js|jsx)$/, /* Identificación de los archivos js y jsx */
         exclude: /node_modules/, /* Excluye la carpeta node_modules */
         use: {
-          loader: "babel-loader" /* Hay que usar el loader para que babel haga el trabajo */
+          loader: 'babel-loader', /* Hay que usar el loader para que babel haga el trabajo */
         },
-      }, 
+      },
       { /* Nueva regla */
         test: /\.html$/, /* Identificación de los archivos html */
         use: {
-          loader: "html-loader" /* Loader de html */
+          loader: 'html-loader', /* Loader de html */
         },
       },
       {
-        test: /\.(s*)css$/,
+        test: /\.(s*)css$/, /* Identificación de los archivos SI ES QUE hay scss y css */
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
           },
-          'css-loader',
-          'sass-loader',
-        ]
+          'css-loader', /* loader de css */
+          'sass-loader', /* loader de sass */
+        ],
       },
     ],
   }, /* Dicta las reglas necesarias para el proyecto */
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html', /* Donde esta ubicado el template */
-      filename: './index.html' /* Nombre */
+      filename: './index.html', /* Nombre */
     }),
     new MiniCssExtractPlugin({
       filename: 'assets/[name].css',
-    })
+    }),
   ], /* Añada los plugins que se necesitan */
-};
+}
